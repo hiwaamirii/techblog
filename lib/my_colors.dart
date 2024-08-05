6ec47b345c3b846632e1bff26e1c418056753dc9
hiwaamirii <hiwaamiri.ir@gmail.com>
2024-08-05 08:40:13 +0330

    1403/5/15 | SolidColors Added.

---
+++ lib/my_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class solidColors{
+  static const Color colorTitle = Color.fromARGB(255, 40, 107, 184);
+  static const Color textTitle = Color.fromARGB(255, 0, 0, 0);
+  static const Color scaffoldBg = Color.fromARGB(255, 255, 255, 255);
+}
+
+class gradiantColors{
+  static const List<Color> bottomNav = [const Color.fromARGB(255, 25, 0, 94), const Color.fromARGB(255, 68, 4, 87)];
+}
