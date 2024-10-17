51e7ba413292703db8f66773e2a3ec110a3fba36
hiwaamirii <hiwaamiri.ir@gmail.com>
2024-10-17 15:33:00 +0330

    hashtag model coded.

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:techblog/gen/assets.gen.dart';
+import 'package:techblog/models/fake_data.dart';
 import 'package:techblog/my_colors.dart';
 
 class MainScreen extends StatelessWidget {
@@ -9,6 +10,7 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
     var size = MediaQuery.of(context).size;
+    double bodyMargin = size.width / 10;
     return SafeArea(
       child: Scaffold(
         body: Padding(
@@ -35,8 +37,9 @@ class MainScreen extends StatelessWidget {
                     decoration: BoxDecoration(
                       borderRadius: const BorderRadius.all(Radius.circular(16)),
                       image: DecorationImage(
-                        image: Image(image: Assets.images.posterTest.provider())
-                            .image,
+                        image: AssetImage(
+                          homePagePosterMap["imageAsset"],
+                        ),
                         fit: BoxFit.cover,
                       ),
                     ),
@@ -57,9 +60,25 @@ class MainScreen extends StatelessWidget {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            Text("ملیکا عزیزی - یک روز پیش",
+                            Text(
+                                homePagePosterMap["writer"] +
+                                    " - " +
+                                    homePagePosterMap["date"],
                                 style: textTheme.titleSmall),
-                            Text("Like 253", style: textTheme.titleSmall),
+                            Row(
+                              children: [
+                                Text(
+                                  homePagePosterMap["view"],
+                                  style: textTheme.titleSmall,
+                                ),
+                                const SizedBox(width: 3),
+                                const Icon(
+                                  Icons.remove_red_eye_sharp,
+                                  size: 16,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
                           ],
                         ),
                         Text("دوازده قدم برنامه نویسی یک دوره ی...",
@@ -69,6 +88,48 @@ class MainScreen extends StatelessWidget {
                   )
                 ],
               ),
+              const SizedBox(height: 16),
+              //tag list
+              SizedBox(
+                height: 60,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: tagList.length,
+                    itemBuilder: ((context, index) {
+                      return Padding(
+                        padding: EdgeInsets.fromLTRB(
+                            0, 8, index == 0 ? bodyMargin : 15, 8),
+                        child: Container(
+                          height: 60,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(24)),
+                            gradient: LinearGradient(
+                              colors: gradiantColors.tags,
+                              begin: Alignment.centerRight,
+                              end: Alignment.centerLeft,
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(16, 8, 8, 8),
+                            child: Row(
+                              children: [
+                                ImageIcon(
+                                  Assets.icons.hashtagicon.provider(),
+                                  color: Colors.white,
+                                  size: 16,
+                                ),
+                                const SizedBox(width: 8),
+                                Text(
+                                  tagList[index].title,
+                                  style: textTheme.headlineMedium,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    })),
+              )
             ],
           ),
         ),
@@ -76,3 +137,4 @@ class MainScreen extends StatelessWidget {
     );
   }
 }
+// 23 دقیقه مانده
---
+++ lib/models/fake_data.dart
@@ -0,0 +1,20 @@
+import 'package:techblog/gen/assets.gen.dart';
+import 'package:techblog/models/data_models.dart';
+
+Map homePagePosterMap = {
+  "imageAsset": Assets.images.posterTest.path,
+  "writer": "ملیکا عزیزی",
+  "date": "یک اروز پیش",
+  "title": "دوازده قدم برنامه نویسی یک دوره",
+  "view": "251",
+};
+
+//blog fake data
+List<HashTagModel> tagList = [
+  HashTagModel(title: "جاوا"),
+  HashTagModel(title: "کاتلین"),
+  HashTagModel(title: "وب"),
+  HashTagModel(title: "هوش مصنوعی"),
+  HashTagModel(title: "iot"),
+  HashTagModel(title: "دارت"),
+];
