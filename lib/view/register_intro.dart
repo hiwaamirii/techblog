0a0a8ae5c1f1e169fc5ef90cd102896ed7b9ea00
hiwaamirii <hiwaamiri.ir@gmail.com>
2024-11-16 21:51:40 +0330

    email validation coded.

--- lib/view/register_intro.dart
+++ lib/view/register_intro.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:techblog/gen/assets.gen.dart';
 import 'package:techblog/my_colors.dart';
 import 'package:techblog/my_strings.dart';
+import 'package:validators/validators.dart';
 
 class RegisterIntro extends StatelessWidget {
   const RegisterIntro({super.key});
@@ -34,53 +35,7 @@ class RegisterIntro extends StatelessWidget {
               padding: const EdgeInsets.only(top: 32),
               child: ElevatedButton(
                 onPressed: () {
-                  showModalBottomSheet(
-                    isScrollControlled: true,
-                      context: context,
-                      backgroundColor: Colors.transparent,
-                      builder: (context) {
-                        return Padding(
-                          padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-                          child: Container(
-                            width: 500,
-                            height: size.height / 2,
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(30),
-                                topRight: Radius.circular(30),
-                              ),
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  MyStrings.insertYourEmail,
-                                  style: textTheme.headlineLarge,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(24),
-                                  child: TextField(
-                                    textAlign: TextAlign.center,
-                                    decoration: InputDecoration(
-                                      hintText: "hiwaamiri.ir@gmail.com",
-                                      hintStyle: textTheme.bodyMedium,
-                                    ),
-                                  ),
-                                ),
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  child: const Text(
-                                    "ادامه",
-                                    style: TextStyle(
-                                        fontFamily: "vazir", color: Colors.white),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      });
+                  _showEmailBottomSheet(context, size, textTheme);
                 },
                 child: const Text(
                   "بزن بریم",
@@ -92,5 +47,63 @@ class RegisterIntro extends StatelessWidget {
         ),
       ),
     ));
+  }
+
+  Future<dynamic> _showEmailBottomSheet(
+      BuildContext context, Size size, TextTheme textTheme) {
+    // RegExp isEmail = RegExp('^[z-z-09]+@');
+    return showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        backgroundColor: Colors.transparent,
+        builder: (context) {
+          return Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Container(
+              width: 500,
+              height: size.height / 2,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    MyStrings.insertYourEmail,
+                    style: textTheme.headlineLarge,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: TextField(
+                      onChanged: (value) {
+                        // print(value + " is Email = " + isEmail.hasMatch(value).toString());
+                        print(value + " is Email : "+ isEmail(value).toString());
+                      },
+                      style: textTheme.headlineLarge,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: "hiwaamiri.ir@gmail.com",
+                        hintStyle: textTheme.bodyMedium,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "ادامه",
+                      style:
+                          TextStyle(fontFamily: "vazir", color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
