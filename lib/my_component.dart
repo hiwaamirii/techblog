29113e5cee3f65cebdd9d25fce7d547a05db3c4c
hiwaamirii <hiwaamiri.ir@gmail.com>
2024-11-18 15:58:50 +0330

    80% of successful registration screen codded.

--- lib/my_component.dart
+++ lib/my_component.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:techblog/gen/assets.gen.dart';
+import 'package:techblog/models/fake_data.dart';
 
 import 'my_colors.dart';
 
@@ -14,6 +16,45 @@ class TechDivider extends StatelessWidget {
       color: SolidColors.dividerColor,
       endIndent: size.width / 6,
       indent: size.width / 6,
+    );
+  }
+}
+
+class MainTags extends StatelessWidget {
+  MainTags({super.key, required this.textTheme, required this.index});
+
+  final TextTheme textTheme;
+  var index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(24)),
+        gradient: LinearGradient(
+          colors: GradiantColors.tags,
+          begin: Alignment.centerRight,
+          end: Alignment.centerLeft,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 8, 8, 8),
+        child: Row(
+          children: [
+            ImageIcon(
+              Assets.icons.hashtagicon.provider(),
+              color: Colors.white,
+              size: 16,
+            ),
+            const SizedBox(width: 8),
+            Text(
+              tagList[index].title,
+              style: textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/view/my_cats.dart
+++ lib/view/my_cats.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:techblog/models/fake_data.dart';
+import 'package:techblog/my_component.dart';
 import 'package:techblog/my_strings.dart';
 
 class MyCats extends StatelessWidget {
@@ -25,14 +27,15 @@ class MyCats extends StatelessWidget {
                     "assets/icons/techbot.png",
                     height: 150,
                   ),
-                   const SizedBox(height: 16),
-                   Text(
+                  const SizedBox(height: 16),
+                  Text(
                     MyStrings.successfulRegistration,
                     textAlign: TextAlign.center,
                     style: textTheme.headlineLarge,
                   ),
                   const SizedBox(height: 16),
-                  TextField(textAlign: TextAlign.center,
+                  TextField(
+                    textAlign: TextAlign.center,
                     decoration: InputDecoration(
                       alignLabelWithHint: true,
                       hintText: "نام و نام خانوادگی",
@@ -44,6 +47,53 @@ class MyCats extends StatelessWidget {
                     MyStrings.chooseCat,
                     textAlign: TextAlign.center,
                     style: textTheme.headlineLarge,
+                  ),
+                  // tag list
+                  Padding(
+                    padding: const EdgeInsets.only(top: 32),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 95,
+                      child: GridView.builder(
+                        physics: const ClampingScrollPhysics(),
+                          itemCount: tagList.length,
+
+                          scrollDirection: Axis.horizontal,
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisSpacing: 5,
+                            mainAxisSpacing: 5,
+                            crossAxisCount: 2,
+                            childAspectRatio: 0.3,
+                          ),
+                          itemBuilder: ((context, index) {
+                            return MainTags(textTheme: textTheme, index: index);
+                          })
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Image.asset("assets/icons/down_cat_arrow.png",scale: 3),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 32),
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 95,
+                      child: GridView.builder(
+                          physics: const ClampingScrollPhysics(),
+                          itemCount: tagList.length,
+
+                          scrollDirection: Axis.horizontal,
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisSpacing: 5,
+                            mainAxisSpacing: 5,
+                            crossAxisCount: 2,
+                            childAspectRatio: 0.3,
+                          ),
+                          itemBuilder: ((context, index) {
+                            return MainTags(textTheme: textTheme, index: index);
+                          })
+                      ),
+                    ),
                   ),
                 ],
               ),
